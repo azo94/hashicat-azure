6af1c848a3eda2058191d83362095e4d93711e5a
azo94 <56064172+azo94@users.noreply.github.com>
2020-11-30 15:59:05 +0900

    Delete vnet.tf

--- vnet.tf
+++
@@ -1,6 +0,0 @@
-module "network" {
-  source  = "app.terraform.io/ljk-training/network/azurerm"
-  version = "3.0.1"
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-  # insert the 1 required variable here
-}
